24b4960308f1681d4f389e0f53107146eaa17593
cyrusmith <info@interosite.ru>
2014-12-11 23:54:48 +0500

    *do not explode war on build

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -85,5 +85,5 @@ grails.project.dependency.resolution = {
     }
 }
 
-grails.project.war.exploded.dir = "target/${appName}-${appVersion}"
-grails.war.exploded=false
+// grails.project.war.exploded.dir = "target/${appName}-${appVersion}"
+// grails.war.exploded=false
